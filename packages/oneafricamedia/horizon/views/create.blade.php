4e1ed6eb5712558c747534699ab471a578b6f57b
datashaman <marlinf@datashaman.com>
2016-05-14 13:31:52 +0200

    Add style

--- packages/oneafricamedia/horizon/views/create.blade.php
+++ packages/oneafricamedia/horizon/views/create.blade.php
@@ -1,5 +1,10 @@
-{!! Form::open([ 'route' => [ 'listing.store' ]]) !!}
+@extends('layout')
+
+@section('content')
+{!! Form::open([ 'route' => [ 'listing.store' ], 'class' => 'pure-form pure-form-stacked' ]) !!}
 {!! Form::hidden('schema', $schema['id']) !!}
+
+<fieldset>
 
 @foreach($schema['properties'] as $property)
 <?php $type = $property['type']; ?>
@@ -7,5 +12,8 @@
 {!! Form::$type($property['id'], null, [ 'class' => 'form-control', 'required' => $property['required'] ? true : null ]) !!}
 @endforeach
 
-{!! Form::submit('Save') !!}
+{!! Form::submit('Save', [ 'class' => 'pure-button pure-button-primary' ]) !!}
+</fieldset>
+
 {!! Form::close() !!}
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+
+</head>
+<body>
+    <div class="content">
+        @yield('content')
+    </div>
+</body>
+</html>
