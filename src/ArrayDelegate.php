e34783cf8b3873279249b3995bb043e07fd9231e
datashaman <marlinf@datashaman.com>
2023-06-15 08:18:30 +0200

    fix array access method signatures

--- src/ArrayDelegate.php
+++ src/ArrayDelegate.php
@@ -4,22 +4,22 @@ namespace Datashaman\Elasticsearch\Model;
 
 trait ArrayDelegate
 {
-    public function offsetSet($offset, $value)
+    public function offsetSet(mixed $offset, mixed $value): void
     {
         array_set($this->{static::$arrayDelegate}, $offset, $value);
     }
 
-    public function offsetExists($offset)
+    public function offsetExists(mixed $offset): bool
     {
         return array_has($this->{static::$arrayDelegate}, $offset);
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset(mixed $offset): void
     {
         array_forget($this->{static::$arrayDelegate}, $offset);
     }
 
-    public function offsetGet($offset)
+    public function offsetGet(mixed $offset): mixed
     {
         return array_get($this->{static::$arrayDelegate}, $offset);
     }
